4f7e9cc0349ff86769936bf2a706e86c72734467
Laura Durinec <laura.djurinec@gmail.com>
2025-11-16 17:09:12 +0100

    Samostalni zadatak za Vježbu 1

--- WA1/app/index.js
+++ WA1/app/index.js
@@ -1,12 +1,34 @@
-const express = require("express");
+import express from "express";
+    //varijabla      //modul 
+
+import path from "path";
+
 const app = express();
 
 const PORT = 3000;
 
+const users = [
+    {id: 1, ime:"Ana", prezime: "Anić"},
+    {id: 2, ime:"Lea", prezime: "Leić"},
+    {id: 3, ime:"Boba", prezime: "Bobić"}
+];
+
+
 app.get('/', (req, res) => {
-    res.send("Hello!");
+    let putanja = path.join('public', 'hello.html');
+    let aps_putanja = path.resolve(putanja);
+    res.sendFile(aps_putanja);
 });
 
+app.get('/about', (req, res) => {
+    let putanja = path.join('public', 'about.html');
+    let aps_putanja = path.resolve(putanja);
+    res.sendFile(aps_putanja);
+});
+
+app.get('/users', (req, res) => {
+    res.json(users); 
+});
 
 app.listen(PORT, error => {
     if (error) {
@@ -14,3 +36,31 @@ app.listen(PORT, error => {
     }
     console.log(`Express poslužitelj sluša na portu ${PORT}`);
 });
+
+
+
+
+/* kod iz skripte
+
+const korisnici = [
+    {id: 1, ime:"Marko", prezime: "Marić"},
+    {id: 2, ime:"Pero", prezime: "Perić"},
+    {id: 3, ime:"Stipe", prezime: "Stipić"}
+];
+
+endpoint: /
+app.get('/', (req, res) => {
+    res.send("Hello!"); // slanje teksta
+});
+
+
+app.get('/', (req, res) => {
+    let putanja = path.join("public", "error.html");
+    let aps_putanja = path.resolve(putanja);
+    res.sendFile(aps_putanja); // slanje HTML-a
+});
+
+app.get('/korisnici', (req, res) => {
+    res.json(korisnici); // slanje JSON-a
+});
+*/
